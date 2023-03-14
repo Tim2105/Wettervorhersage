71c92f38bc15410b1add06beb3b7807c485ccebe
Nils <45481034+NiBa2018@users.noreply.github.com>
2023-03-14 12:28:16 +0100

    Charts

--- src/routes/details/+page.svelte
+++ src/routes/details/+page.svelte
@@ -1,67 +1,97 @@
-<!-- Die Detail.svelte soll mit  chart js ein Diagramm den Temperaturverlauf des Tages zeichnen  -->
-<div class="Charts">
-<span class="chartBox">
-    <canvas id='Temperatur'></canvas>
-</span>
-<span class="chartBox">
-    <canvas id='Regen'></canvas>
-</span>
-<span class="chartBox">
-    <canvas id='Wind'></canvas>
-</span>
-</div>
 <script>
+    import chartjs from "chart.js/auto";
+    import store from "/src/stores/store.js";
+    import { onMount } from "svelte";
 
-	import chartjs from 'chart.js/auto';
-    import store from '/src/stores/store.js';
-	import { onMount } from 'svelte';
+    const chartLabels = [
+        0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
+        20, 21, 22, 23,
+    ];
+    let ctx;
+    let chartCanvas;
 
-
-	const chartLabels = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23];
-	let ctx;
-	let chartCanvas;
-
-	onMount(async (promise) => {
-        printChart(store.temperature, 'Temperatur');
-        printChart(store.rain, 'Regen');
-        printChart(store.wind, 'Wind');
-		
+    onMount(async (promise) => {
+        printChart(
+            store.temperature,
+            "Temperatur",
+            "rgb(255, 99, 132)",
+            "Temperatur",
+            "Stunden"
+        );
+        printChart(
+            store.rain,
+            "Regen",
+            "rgb(54, 162, 235)",
+            "Regenmenge",
+            "Stunden"
+        );
+        printChart(
+            store.wind,
+            "Wind",
+            "rgb(255, 205, 86)",
+            "Windstärke",
+            "Stunden"
+        );
     });
 
-    function printChart(array , chartname) {
+    function printChart(array, chartname, colour, yAchse, xAchse) {
         console.log(store.temperature);
-        ctx = chartCanvas.getContext('2d');
-			var rain = new chartjs(document.getElementById(chartname), {
-				type: 'line',
-				data: {
-						labels: chartLabels,
-						datasets: [{
-								label: chartname,
-								backgroundColor: 'rgb(255, 99, 132)',
-								borderColor: 'rgb(255, 99, 132)',
-								data: array
-						}]
-                
-				},options: {
-                    responsive: false
-                }
-		});
+        ctx = chartCanvas.getContext("2d");
+        //ctx.canvas.width = 300;
+        //ctx.canvas.height = 300;
+        var rain = new chartjs(document.getElementById(chartname), {
+            type: "line",
+            data: {
+                labels: chartLabels,
+                datasets: [
+                    {
+                        label: chartname,
+                        fill: true,
+                        backgroundColor: colour,
+                        borderColor: "rgb(0,0,0)",
+                        data: array,
+                    },
+                ],
+            },
+            options: {
+                scales: {
+                    y: {
+                        display: true,
+                        text: yAchse,
+                    },
+                    x: {
+                        display: true,
+                        text: xAchse,
+                    },
+                },
+                responsive: false,
+            },
+        });
     }
 </script>
 
-<canvas bind:this={chartCanvas} id="chart"></canvas> 
+<div class="Charts">
+    <span class="chartBox">
+        <canvas id="Temperatur" />
+    </span>
+    <span class="chartBox">
+        <canvas id="Regen" />
+    </span>
+    <span class="chartBox">
+        <canvas id="Wind" />
+    </span>
+</div>
+
+<canvas bind:this={chartCanvas} id="chart" />
 
 <style>
     .Charts {
         display: flex;
         flex-direction: row;
-        justify-content: space-around;
         align-items: center;
-        width: 100%;
-        height: 100%;
     }
-    .chartBox {
-        width: 30%;
-        height: 30%;
+    canvas {
+        width: 750px;
+        height: 750px;
     }
 </style>
